adb5439bbcdb4e64bc45c46bc10db3309d83b867
alafezai <fezaiala650@gmail.com>
2021-12-28 14:37:22 +0100

    header home screen

--- lib/home/composent/body.dart
+++ lib/home/composent/body.dart
@@ -8,15 +8,48 @@ class Body extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            Row(
-              children: [
-                Container(
-                  width: 200,
-                  height: 50,
-                  color: Colors.black,
-                )
-              ],
-            )
+            Padding(
+                // ignore: prefer_const_constructors
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: TextField(
+                          onChanged: (value) => {
+                                //serch value
+                              },
+                          // ignore: prefer_const_constructors
+                          decoration: InputDecoration(
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              hintText: "Search Product",
+                              prefixIcon:
+                                  Image.asset("assets/images/serc.PNG"))),
+                    ),
+                    Container(
+                      width: 46,
+                      height: 46,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          shape: BoxShape.circle),
+                      child: Image.asset("assets/images/panier.PNG"),
+                    ),
+                    Container(
+                      width: 46,
+                      height: 46,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          shape: BoxShape.circle),
+                      child: Image.asset("assets/images/not.PNG"),
+                    )
+                  ],
+                )),
           ],
         ),
       ),
